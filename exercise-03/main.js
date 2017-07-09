a701f3256f31e15459b3106f2d18207d0fee1720
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2017-07-09 10:18:17 +0800

    Add exercise-03 - Creating your first APi

---
+++ exercise-03/main.js
@@ -0,0 +1,31 @@
+// Exercise 03 - Creating your first API
+
+// import our mock data
+const data = require('./mocks/data.json');
+
+// import express framework
+const express = require('express');
+
+// import body-parser that is used by express framework
+// to determine how it would interpret incoming data
+const bodyParser = require('body-parser');
+
+// initialize express
+const app = express();
+
+// tell express to parse HTTP request bodies as JSON
+const jsonParser = bodyParser.json();
+
+// create a route for /api/customers
+app.get('/api/customers', jsonParser, (req, res) => {
+  res.status(200); // set 200 (OK) status
+  res.send(data); // send response
+});
+
+// get port from environment variable PORT (defaults to 9999)
+const port = process.env.PORT || 9999;
+
+// start API
+app.listen(port, () => {
+  console.log(`You are now listening to http://localhost:${port}`);
+});
