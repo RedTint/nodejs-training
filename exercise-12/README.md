263675968b1085938aa9370ca435aca538d980cd
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2017-07-16 05:57:52 +0800

    Add exercise 12, creating a logger using winston

---
+++ exercise-12/README.md
@@ -0,0 +1,11 @@
+# Exercise 12 - Creating a logger using winston
+
+### Tech Story
+Setup a winston logger that can be used in your application
+
+### Assumptions
+- You can define your own filename for logs
+
+### UAC
+- Demo that logger displays logs in console
+- Demo that logger stores logs in a file
---
+++ exercise-12/main.js
@@ -0,0 +1,38 @@
+// load libraries
+const winston = require('winston');
+const express = require('express');
+
+// configure Winston logger
+// define transports
+// tranport - methods of logging (e.g: console logs, file logs, 3rd-party API logs, etc.)
+const tranports = [
+    // display logs in console
+    new (winston.transports.Console)(),
+    // store logs in { filename: /path/to/file }
+    new (winston.transports.File)({ filename: 'somefile.log' })
+];
+// instantiating winston logger
+var logger = new (winston.Logger)({
+    transports: tranports
+});
+
+// instantiate app
+const app = express();
+
+// create some route we can listen to and test the logs
+app.get('/', (req, res) => {
+    // use logger
+    logger.log('info', `This is an 'info' log`);
+    logger.log('error', `This is an 'error' log`);
+    res.send('Ongoing: testing logs');
+})
+
+// create some logs every 5000 seconds
+setInterval(() => {
+    logger.log('info', 'Logging every 5 seconds');
+}, 5000)
+
+const port = 3000;
+app.listen(3000, () => {
+    logger.log('info', `Listening to port ${port}`);
+});
